a57def46dec265c4075218f51396db1e0f426dde
dev <dev@gmail.com>
2022-08-26 23:16:43 +0530

    changed app using express generator

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ var logger = require('morgan');
 
 var app = express();
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'web')));
 
+
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
@@ -27,6 +30,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 
 module.exports = app;
